2cf36079da63a16024b8d34e93665ad5e86eb9d8
Likhith Raju1 <likhith.raju1@ibm.com>
2023-02-09 23:18:42 +0530

    Likhith-Changes-Context

--- src/component/Meals/MealItemForm.js
+++ src/component/Meals/MealItemForm.js
@@ -1,16 +1,16 @@
-import classes from './MealItemForm.module.css';
-import { useRef } from 'react';
+import classes from "./MealItemForm.module.css";
+import { useRef } from "react";
 const MealItemForm = (props) => {
-  const addItemClicked = (event) =>{
-    event.preventDefault()
-    props.addToCart(amountref.current.value)
-    
-  }
-  const amountref = useRef()
+  const addItemClicked = (event) => {
+    event.preventDefault();
+    props.addToCart(amountref.current.value);
+    amountref.current.value = 0;
+  };
+  const amountref = useRef();
   return (
-    <form className={classes.form} onSubmit = {addItemClicked}>
+    <form className={classes.form} onSubmit={addItemClicked}>
       <label>AMOUNT</label>
-      <input type="number" min='1' max='5' ref={amountref} />
+      <input type="number" min="1" max="5" ref={amountref} />
       <button>ADD</button>
     </form>
   );
